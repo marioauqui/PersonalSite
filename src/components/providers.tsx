12ac8bdcfccd676aa042bbcbfbdfc077b8104d45
marioauqui <auqumo21@wfu.edu>
2025-10-10 21:11:59 -0400

    feat: initial site + dark mode wiring (toggle still stuck; see README)

---
+++ src/components/providers.tsx
@@ -0,0 +1,17 @@
+// src/components/providers.tsx
+"use client";
+
+import { ThemeProvider } from "next-themes";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+}
